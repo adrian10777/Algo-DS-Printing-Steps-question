002d305fc04f33f908345f7b1925336d76eb9d5a
Adrian Henriquez <adrianhenriquez@Adrians-MacBook-Pro.local>
2022-03-01 11:45:20 -0500

    secondcommit

--- index.js
+++ index.js
@@ -32,6 +32,22 @@
     }
 } */
 
-function steps(n) {
-    
+
+function steps(n, row = 0, stair = '') {
+    if ( n === row ) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    /* if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    } */
+    // break this down^
+    const add = stair.length <= row ? '#' : ' '
+    steps(n, row, stair + add);
 }
